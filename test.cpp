7b4fe460f376fbe8f5aaa106481a8cc6e9ff0332
harleynghx <harleynghx.edu@gmail.com>
2025-06-16 02:34:26 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harleyng <harleyng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 01:57:11 by harleyng          #+#    #+#             */
+/*   Updated: 2025/06/16 02:03:07 by harleyng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
