5812dfec41c5f17c078a266353b96a7605f4471b
harleynghx <harleynghx.edu@gmail.com>
2025-06-17 22:50:42 +0800

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char** argv) {
+    std::string output;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg(argv[i]);
+        for (std::size_t j = 0; j < arg.length(); ++j)
+            arg[j] = std::toupper(arg[j]);
+        output += arg;
+    }
+    if (output.empty())
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+        std::cout << output << std::endl;
+    return 0;
+}
