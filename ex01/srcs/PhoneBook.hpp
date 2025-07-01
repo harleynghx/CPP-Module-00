f7d7ea53acbc40075b3c21dcab0c1045be8043f2
harleynghx <harleynghx.edu@gmail.com>
2025-07-01 20:12:48 +0800

    rewrote

--- ex01/srcs/PhoneBook.hpp
+++ ex01/srcs/PhoneBook.hpp
@@ -4,21 +4,21 @@
 #include "Contact.hpp"
 
 class PhoneBook {
+public:
+    PhoneBook(); // Constructor
+    ~PhoneBook(); // Destructor
+
+    void addContact();
+    void searchContact() const;
+
 private:
-    Contact contacts[8];
+    Contact contacts[0];
     int contactCount;
     int oldestContactIndex;
 
     std::string truncateString(const std::string &str) const;
     void displayContactTableHeader() const;
     void displayContactTableRow(int index) const;
-
-public:
-    PhoneBook();
-    ~PhoneBook();
-
-    void addContact();
-    void searchContact() const;
 };
 
 #endif
