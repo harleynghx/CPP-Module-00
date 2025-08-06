42930dca9b4102e392fc7980c58ef46d6e94e30c
harleynghx <harleynghx.edu@gmail.com>
2025-08-06 20:03:30 +0800

    ex02

---
+++ ex02/test2.cpp
@@ -0,0 +1,19 @@
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts() { return _nbAccounts; }
+int Account::getTotalAmount() { return _totalAmount; }
+int Account::getNbDeposits() { return _totalNbDeposits; }
+int Account::getNbWithdrawals() { return _totalNbWithdrawals; }
+
+void Account::displayAccountsInfos() {
+    _displayTimestamp();
+    std::cout << 
+}
